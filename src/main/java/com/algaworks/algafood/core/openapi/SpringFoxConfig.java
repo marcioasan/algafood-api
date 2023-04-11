9fd8a6264be67f76373da07e08ac4a5789bb9f4e
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-04-10 21:43:13 -0300

    18.15. Referenciando modelo de representação de problema com códigos de status de erro

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,13 +2,14 @@ package com.algaworks.algafood.core.openapi;
 
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
@@ -18,6 +19,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -70,6 +72,8 @@ public class SpringFoxConfig {
 		      new ResponseBuilder()
 		          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
 		          .description("Erro interno do Servidor")
+		          .representation(MediaType.APPLICATION_JSON) //18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+		          .apply(getProblemaModelReference())
 		          .build(),
 		      new ResponseBuilder()
 		          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -83,10 +87,14 @@ public class SpringFoxConfig {
 		        new ResponseBuilder()
 		            .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
 		            .description("Requisição inválida (erro do cliente)")
+			        .representation(MediaType.APPLICATION_JSON) //18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+			        .apply(getProblemaModelReference())		            
 		            .build(),
 		        new ResponseBuilder()
 		            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
 		            .description("Erro interno no servidor")
+			        .representation(MediaType.APPLICATION_JSON) //18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+			        .apply(getProblemaModelReference())
 		            .build(),
 		        new ResponseBuilder()
 		            .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -95,6 +103,8 @@ public class SpringFoxConfig {
 		        new ResponseBuilder()
 		            .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
 		            .description("Requisição recusada porque o corpo está em um formato não suportado")
+			        .representation(MediaType.APPLICATION_JSON) //18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+			        .apply(getProblemaModelReference())
 		            .build()
 		    );
 	  }
@@ -105,10 +115,14 @@ public class SpringFoxConfig {
 		        new ResponseBuilder()
 		            .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
 		            .description("Requisição inválida (erro do cliente)")
+			        .representation(MediaType.APPLICATION_JSON) //18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+			        .apply(getProblemaModelReference())
 		            .build(),
 		        new ResponseBuilder()
 		            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
 		            .description("Erro interno no servidor")
+			        .representation(MediaType.APPLICATION_JSON) //18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+			        .apply(getProblemaModelReference())
 		            .build()
 		    );
 	  }
@@ -121,6 +135,13 @@ public class SpringFoxConfig {
 				.version("1")
 				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
 				.build();
+	}
+	
+	//18.15. Referenciando modelo de representação de problema com códigos de status de erro - Ver conteúdo de apoio
+	private Consumer<RepresentationBuilder> getProblemaModelReference() {
+	    return r -> r.model(m -> m.name("Problema")
+	            .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+	                    q -> q.name("Problema").namespace("com.algaworks.algafood.api.exceptionhandler")))));
 	}
 	
 }
